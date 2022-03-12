0b3e4a1a52e7e2f8a9f5536e2cdf635f3aa862b8
IIT2021117 <rajput.rishika321@gmail.com>
2022-03-13 04:43:21 +0530

    corrected

--- jobSearch/JobSearch.js
+++ jobSearch/JobSearch.js
@@ -1,25 +1,27 @@
-import { jobTemplate } from './templates';
-import { extractFormData, getCurrencySymbol } from './utils';
-
+import { jobTemplate } from "./templates";
+import { extractFormData, getCurrencySymbol } from "./utils";
 
 export class JobSearch {
-
-  constructor(searchFormSelector, resultsContainerSelector, loadingElementSelector) {
+  constructor(
+    searchFormSelector,
+    resultsContainerSelector,
+    loadingElementSelector
+  ) {
     this.searchForm = document.querySelector(searchFormSelector);
     this.resultsContainer = document.querySelector(resultsContainerSelector);
     this.loadingElement = document.querySelector(loadingElementSelector);
   }
 
   setCountryCode() {
-    this.countryCode = 'in';
+    this.countryCode = "in";
     this.setCurrencySymbol();
 
-    fetch('http://ip-api.com/json')
-      .then(results => results.json())
-      .then(results => {
-        this.countryCode = results.countryCode.toLowerCase();
-        this.setCurrencySymbol();
-      });
+    // fetch("http://ip-api.com/json")
+    //   .then((results) => results.json())
+    //   .then((results) => {
+    //     this.countryCode = results.countryCode.toLowerCase();
+    //     this.setCurrencySymbol();
+    //   });
   }
 
   setCurrencySymbol() {
@@ -27,29 +29,31 @@ export class JobSearch {
   }
 
   configureFormListener() {
-    this.searchForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    this.startLoading();
-    this.resultsContainer.innerHTML = '';
-    const { search, location } = extractFormData(this.searchForm);
-    fetch(`https://infinite-meadow-74063.herokuapp.com/?search=${search}&location=${location}&country=${this.countryCode}`)
-      .then(response => response.json())
-      .then(({ results }) => {
-        this.stopLoading();
-        return results
-          .map(job => jobTemplate(job, this.currencySymbol))
-          .join('');
-      })
-      .then(jobs => this.resultsContainer.innerHTML = jobs)
-      .catch(() => this.stopLoading());
+    this.searchForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      this.startLoading();
+      this.resultsContainer.innerHTML = "";
+      const { search, location } = extractFormData(this.searchForm);
+      fetch(
+        `http://localhost:3000/?search=${search}&location=${location}&country=${this.countryCode}`
+      )
+        .then((response) => response.json())
+        .then(({ results }) => {
+          this.stopLoading();
+          return results
+            .map((job) => jobTemplate(job, this.currencySymbol))
+            .join("");
+        })
+        .then((jobs) => (this.resultsContainer.innerHTML = jobs))
+        .catch(() => this.stopLoading());
     });
   }
 
   startLoading() {
-    this.loadingElement.classList.add('loading');
+    // this.loadingElement.classList.add("loading");
   }
 
   stopLoading() {
-    this.loadingElement.classList.remove('loading');
+    // this.loadingElement.classList.remove("loading");
   }
 }
